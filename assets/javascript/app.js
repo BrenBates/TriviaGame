8f421af98e0c6690af40f59686db64adbc798a96
Bren <brennenbates2859@gmail.com>
2019-09-06 11:21:25 -0600

    Update app.js
    initial javascript.  Added a timer after learning about setInterval and clearInterval.  Adding some gif links to use after each question is answered.

---
+++ assets/javascript/app.js
@@ -0,0 +1,51 @@
+
+
+ $(document).on('click', 'button', function() {
+    timeVar = setInterval(myTimer, 1000);
+ });
+ 
+ var timer = 5;
+  $('#countDown').empty().append(timer);
+
+  function myTimer() {
+    timer = timer-1;
+    $('#countDown').empty().append(timer);
+
+    if (timer <= 0) {
+        clearInterval(timeVar);
+    }
+
+  }
+
+$('#gifSpace').append('');
+
+
+//Final Fantasy opener
+//<div class="tenor-gif-embed" data-postid="11029638" data-share-method="host" data-width="100%" data-aspect-ratio="2.083682008368201"><a href="https://tenor.com/view/final-fantasy-gif-11029638">Final Fantasy GIF</a> from <a href="https://tenor.com/search/finalfantasy-gifs">Finalfantasy GIFs</a></div><script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+
+//Sephiroth gif
+// <div class="tenor-gif-embed" data-postid="5802543" data-share-method="host" data-width="100%" data-aspect-ratio="1.3333333333333333"><a href="https://tenor.com/view/sephiroth-final-fantasy7-ff7-flames-burn-gif-5802543">Sephiroth Final Fantasy7 GIF</a> from <a href="https://tenor.com/search/sephiroth-gifs">Sephiroth GIFs</a></div><script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+
+//Sabin train suplex 
+// <div class="tenor-gif-embed" data-postid="12127442" data-share-method="host" data-width="100%" data-aspect-ratio="1.1531531531531531"><a href="https://tenor.com/view/ffvi-suplex-sabin-train-phantom-train-gif-12127442">Ffvi Suplex GIF</a> from <a href="https://tenor.com/search/ffvi-gifs">Ffvi GIFs</a></div><script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+
+//Cactuar 
+//<div class="tenor-gif-embed" data-postid="12386155" data-share-method="host" data-width="100%" data-aspect-ratio="1.0"><a href="https://tenor.com/view/cactuar-final-fantasy-ffs-gamer-gif-12386155">Cactuar Final Fantasy GIF</a> from <a href="https://tenor.com/search/cactuar-gifs">Cactuar GIFs</a></div><script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+
+//Yuna 
+//<div class="tenor-gif-embed" data-postid="14602022" data-share-method="host" data-width="100%" data-aspect-ratio="1.7785714285714287"><a href="https://tenor.com/view/ffx-yuna-final-fantasy-x-dance-gif-14602022">Ffx Yuna GIF</a> from <a href="https://tenor.com/search/ffx-gifs">Ffx GIFs</a></div><script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+
+//Rikku
+//<div class="tenor-gif-embed" data-postid="14806446" data-share-method="host" data-width="100%" data-aspect-ratio="1.693877551020408"><a href="https://tenor.com/view/rikku-how-you-doing-final-fantasy-x-gif-14806446">Rikku How You Doing GIF</a> from <a href="https://tenor.com/search/rikku-gifs">Rikku GIFs</a></div><script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+
+//Squall
+//<div class="tenor-gif-embed" data-postid="12309258" data-share-method="host" data-width="100%" data-aspect-ratio="1.2835051546391754"><a href="https://tenor.com/view/final-fantasy8-rinoa-squall-ff8-funny-gif-12309258">Final Fantasy8 Rinoa GIF</a> from <a href="https://tenor.com/search/finalfantasy8-gifs">Finalfantasy8 GIFs</a></div><script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+
+//Final Fantasy 9
+//<div class="tenor-gif-embed" data-postid="5548544" data-share-method="host" data-width="100%" data-aspect-ratio="1.326086956521739"><a href="https://tenor.com/view/alexandros-final-fantasy9-gif-5548544">Alexandros Final Fantasy9 GIF</a> from <a href="https://tenor.com/search/alexandros-gifs">Alexandros GIFs</a></div><script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+
+//Black Mage
+//<div class="tenor-gif-embed" data-postid="11823290" data-share-method="host" data-width="100%" data-aspect-ratio="1.0"><a href="https://tenor.com/view/final-fantasy-wizard-magic-gif-11823290">Final Fantasy Wizard GIF</a> from <a href="https://tenor.com/search/finalfantasy-gifs">Finalfantasy GIFs</a></div><script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+
+//Lightning 
+//<div class="tenor-gif-embed" data-postid="11068056" data-share-method="host" data-width="100%" data-aspect-ratio="1.711340206185567"><a href="https://tenor.com/view/final-fantasy-lightning-gif-11068056">Final Fantasy Lightning GIF</a> from <a href="https://tenor.com/search/finalfantasy-gifs">Finalfantasy GIFs</a></div><script type="text/javascript" async src="https://tenor.com/embed.js"></script>
