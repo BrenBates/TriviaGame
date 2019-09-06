346f9f0385ec746bac8da375a6012b9055f45d84
Bren <brennenbates2859@gmail.com>
2019-09-06 14:24:40 -0600

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,148 @@
+$(document).ready(function() {
+
+////////////////////////
+//Questions as Objects//
+////////////////////////
 
 
- $(document).on('click', 'button', function() {
-    timeVar = setInterval(myTimer, 1000);
+
+
+var questionOne = {
+    "text" : "Which iconic villain aimed to become a god by merging with the lifestream of the planet?",
+    "optionOne" : '<br><button>Golbez</button>',
+    "optionTwo" : "<br><button>Kefka</button>",
+    "optionThree" : "<br><button class = 'correct'>Sephiroth</button>", 
+    "optionFour" : "<br><button>Halicarnassus</button>",
+    "answer" : "Sephiroth",
+    "gif" : '<div class="tenor-gif-embed" data-postid="5802543" data-share-method="host" data-width="100%" data-aspect-ratio="1.3333333333333333"><a href="https://tenor.com/view/sephiroth-final-fantasy7-ff7-flames-burn-gif-5802543">Sephiroth Final Fantasy7 GIF</a> from <a href="https://tenor.com/search/sephiroth-gifs">Sephiroth GIFs</a></div><script type="text/javascript" async src="https://tenor.com/embed.js"></script>'
+};
+
+
+
+var questionTwo = {
+    "text" : "This iconic villain aimed to become a god by merging with the lifestream of the planet.  One  minor detail this requires smashing a meteor into the planet",
+    "optionOne" : '<button>1</button>',
+    "optionTwo" : "<button>2</button>",
+    "optionThree" : "<button class = 'correct'>3</button>", 
+    "optionFour" : "<button>4</button>",
+    "gif" : '<div class="tenor-gif-embed" data-postid="5802543" data-share-method="host" data-width="100%" data-aspect-ratio="1.3333333333333333"><a href="https://tenor.com/view/sephiroth-final-fantasy7-ff7-flames-burn-gif-5802543">Sephiroth Final Fantasy7 GIF</a> from <a href="https://tenor.com/search/sephiroth-gifs">Sephiroth GIFs</a></div><script type="text/javascript" async src="https://tenor.com/embed.js"></script>'
+};
+
+
+var questionThree = {
+    "text" : "This iconic villain aimed to become a god by merging with the lifestream of the planet.  One  minor detail this requires smashing a meteor into the planet",
+    "optionOne" : '<button>Golbez</button>',
+    "optionTwo" : "<button>Kefka</button>",
+    "optionThree" : "<button class = 'correct'>Sephiroth</button>", 
+    "optionFour" : "<button>Halicarnassus</button>",
+    "gif" : '<div class="tenor-gif-embed" data-postid="5802543" data-share-method="host" data-width="100%" data-aspect-ratio="1.3333333333333333"><a href="https://tenor.com/view/sephiroth-final-fantasy7-ff7-flames-burn-gif-5802543">Sephiroth Final Fantasy7 GIF</a> from <a href="https://tenor.com/search/sephiroth-gifs">Sephiroth GIFs</a></div><script type="text/javascript" async src="https://tenor.com/embed.js"></script>'
+};
+
+var questionFour = {
+    "text" : "This iconic villain aimed to become a god by merging with the lifestream of the planet.  One  minor detail this requires smashing a meteor into the planet",
+    "optionOne" : '<button>Golbez</button>',
+    "optionTwo" : "<button>Kefka</button>",
+    "optionThree" : "<button class = 'correct'>Sephiroth</button>", 
+    "optionFour" : "<button>Halicarnassus</button>",
+    "gif" : '<div class="tenor-gif-embed" data-postid="5802543" data-share-method="host" data-width="100%" data-aspect-ratio="1.3333333333333333"><a href="https://tenor.com/view/sephiroth-final-fantasy7-ff7-flames-burn-gif-5802543">Sephiroth Final Fantasy7 GIF</a> from <a href="https://tenor.com/search/sephiroth-gifs">Sephiroth GIFs</a></div><script type="text/javascript" async src="https://tenor.com/embed.js"></script>'
+};
+
+
+
+
+//Global variables
+questions = [questionOne, questionTwo, questionThree, questionFour];
+questionIndex = 0;
+correctCount = 0; 
+let questionAnswered = false;
+let timer = 30;
+let contentDiv = $('<div>');
+let msgSpace = $('<p>');
+let counterP = $('<p>');
+let ffHeader = $('<h1>');
+ffHeader.append('Final Fantasy Trivia');
+let gifSpace = $('<div>');
+ counterP.attr('id','countDown');
+ msgSpace.attr('id','msg');
+ gifSpace.attr('id','gifSpace');
+contentDiv.append(ffHeader).append(counterP).append(msgSpace);
+
+
+//Main process
+
+ $(document).on('click', '#startGame', function() {
+
+
+    $('.container').css('background-color','lightblue');
+    $('.container').empty();
+    $('#msg').empty();
+    $('#gifSpace').empty().children().remove();
+    $('.container').append(contentDiv).append(gifSpace);
+    x();
+    
  });
  
- var timer = 5;
-  $('#countDown').empty().append(timer);
 
-  function myTimer() {
-    timer = timer-1;
-    $('#countDown').empty().append(timer);
+ //functions
 
-    if (timer <= 0) {
+ let x = function nextQuestion() {
+
+console.log(questionIndex);
+questionAnswered = false;
+$('#msg').empty();
+$('#gifSpace').empty();
+
+ $('#msg').append(questions[questionIndex].text);
+ $('#msg').append(questions[questionIndex].optionOne);
+ $('#msg').append(questions[questionIndex].optionTwo);
+ $('#msg').append(questions[questionIndex].optionThree);
+ $('#msg').append(questions[questionIndex].optionFour);
+
+
+ timeVar = setInterval(myTimer, 1000);
+
+ $(document).on('click', 'button', function() {
+
+     
+
+     if( $(this).hasClass('correct') === true && questionAnswered === false ) {
+         $('#msg').empty().append('That is correct!');
+         correctCount += 1;
+         questionIndex += 1;
+         $('#gifSpace').append(questions[questionIndex].gif);
+         clearInterval(timeVar);
+         questionAnswered = true;
+         
+     }
+
+     if( $(this).hasClass('correct') === false  && questionAnswered === false ) {
+        $('#msg').empty().append('Incorrect! The correct answer is ' + questions[questionIndex].answer + '.');
+        questionIndex += 1;
+        $('#gifSpace').append(questions[questionIndex].gif);
         clearInterval(timeVar);
+        questionAnswered = true;
+        
     }
 
-  }
+    if(questionAnswered === true && questionIndex < questions.length) {
+        return setTimeout(x, 2000);
+    }
 
-$('#gifSpace').append('');
+ });
+
+}
+
+
+
+function myTimer() {
+timer = timer-1;
+ $('#countDown').empty().append('Time remaining: ' + timer);
+
+ if (timer <= 0) {
+     clearInterval(timeVar);
+   }
+
+  }
 
 
 //Final Fantasy opener
@@ -49,3 +174,5 @@ $('#gifSpace').append('');
 
 //Lightning 
 //<div class="tenor-gif-embed" data-postid="11068056" data-share-method="host" data-width="100%" data-aspect-ratio="1.711340206185567"><a href="https://tenor.com/view/final-fantasy-lightning-gif-11068056">Final Fantasy Lightning GIF</a> from <a href="https://tenor.com/search/finalfantasy-gifs">Finalfantasy GIFs</a></div><script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+
+});
